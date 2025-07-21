065c0efadb84c5e762b3d2b1910d4b9127ac04cc
LuccasAvelar <94063002+LuccasAvelar@users.noreply.github.com>
2025-07-21 10:39:34 -0300

    Delete Desktop/Apps Sonax Conector/Sonax Conector Hubspot/Hubspot v1.2 directory

--- Desktop/Apps Sonax Conector/Sonax Conector Hubspot/Hubspot v1.2/src/app/app.functions/example-function.js
+++
@@ -1,9 +0,0 @@
-const axios = require("axios")
-
-exports.main = async (context = {}) => {
-  const { text } = context.parameters
-
-  const response = `This is coming from a serverless function! You entered: ${text}`
-
-  return response
-}
